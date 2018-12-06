e2039f3e59d6142d55cb8135caad1f2b6fa10b21
Pavel Vovk <capability@capability.org.ua>
2018-12-06 15:09:35 +0200

    add summ algoritm

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,6 +10,20 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello world ");
+            Calculator.Add(2, 3);
+            Console.ReadLine();
+        }
+    }
+
+    class Calculator
+    {
+        public static void Add(int a, int b)
+        {
+            int summ = a + b;
+
+            Console.WriteLine($"Сумма {a} и {b} равна: {summ}");
+            Console.WriteLine("hi");
         }
     }
 }
