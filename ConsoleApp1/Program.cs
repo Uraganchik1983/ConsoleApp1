2530150f86950fd46d80591400f2e9c56a47881f
Pavel Vovk <capability@capability.org.ua>
2018-12-07 18:55:49 +0200

    add example Prazryadn operation, Preobrazovanie tipov

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,8 +10,22 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world ");
-            Calculator.Add(2, 3);
+            Console.WriteLine("Арифметические операции: ");
+            Console.WriteLine("Введите X: ");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите Y: ");
+            int y = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine();
+            Calculator.Add(x, y);
+
+            Console.WriteLine();
+            Console.WriteLine("Поразрядные операции: ");
+            Razryad.Sum(x, y);
+
+            Console.WriteLine();
+            Console.WriteLine("Преобразование типов бывает: расширение, сужение. Пример byte x = (byte)(5+70)");
+
             Console.ReadLine();
         }
     }
@@ -21,9 +35,21 @@ namespace ConsoleApp1
         public static void Add(int a, int b)
         {
             int summ = a + b;
-
             Console.WriteLine($"Сумма {a} и {b} равна: {summ}");
-            Console.WriteLine("hi");
+        }
+    }
+
+    class Razryad
+    {
+        public static void Sum (int x, int y)
+        {
+            Console.WriteLine($"Двоичный код {x} = " + Convert.ToString(x, 2));
+            Console.WriteLine($"Двоичный код {y} = " + Convert.ToString(y, 2));
+            Console.WriteLine("x & y =  " + (x & y));
+            Console.WriteLine("x | y = " + (x | y));
+            Console.WriteLine("x ^ y = " + (x ^ y));
+            Console.WriteLine("x << y = " + (x << y));
+            Console.WriteLine("x >> y = " + (x >> y));
         }
     }
 }
